0a921955bd597a8a14d4b929a0b6de507a59f58a
devudaya <niroshan127@gmail.com>
2021-09-20 20:47:48 +0530

    first commit

---
+++ Shared/M1AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  M1AppApp.swift
+//  Shared
+//
+//  Created by Udaya Niroshana on 2021-09-19.
+//
+
+import SwiftUI
+
+@main
+struct M1AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
